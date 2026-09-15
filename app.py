a3adc02fe5bb0d759938f8a0437a8030f326b7d0
Bahula21 <bahulakbangera@gmail.com>
2026-09-15 19:34:42 +0530

    Learn request.form and handle form input

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template, request
 
 app = Flask(__name__)
 
-@app.route("/<short_code>")
-def redirect(short_code):
-    return short_code
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/shorten", methods=["POST"])
+def shorten():
+    long_url=request.form["long_url"]
+    return long_url
 
 if __name__ == "__main__":
     app.run(debug=True)
