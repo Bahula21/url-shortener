2af4495e300254870f80020ae2ab335d9f3b1f0c
Bahula21 <bahulakbangera@gmail.com>
2026-09-15 20:16:38 +0530

    Add database initialization

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask,render_template, request
+import sqlite3
 
 app = Flask(__name__)
+
+def get_db():
+    return sqlite3.connect("urls.db")
+
+def init_db():
+    connection = get_db()
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS urls(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            short_code TEXT UNIQUE,
+            long_url TEXT
+        )
+    """)
+
+    connection.commit()
+    connection.close()
 
 @app.route("/")
 def home():
@@ -10,6 +29,8 @@ def home():
 def shorten():
     long_url=request.form["long_url"]
     return long_url
+
+init_db()
 
 if __name__ == "__main__":
     app.run(debug=True)
